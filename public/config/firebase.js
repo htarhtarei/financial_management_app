15773ecc82c04d999a8b0445aa77de0761084cec
htarhtarei <120170767+htarhtarei@users.noreply.github.com>
2025-03-20 22:06:19 +0630

    install: react-query and firebase

---
+++ public/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDLGz24Hia2LZKGor_fA6ek_v185Z950DQ",
+    authDomain: "finance-management-7373c.firebaseapp.com",
+    projectId: "finance-management-7373c",
+    storageBucket: "finance-management-7373c.firebasestorage.app",
+    messagingSenderId: "703026148771",
+    appId: "1:703026148771:web:5977195982b9beb80cd616",
+    measurementId: "G-CQCQ4VD60C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
